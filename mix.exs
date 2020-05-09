1c9a10497febad059b27d9c72373b68ba6fb9a25
Yuri Kovalov <yuri@yurikoval.com>
2020-05-09 12:25:13 +0800

    Add dialyzer

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule ExTectonicdb.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: dialyzer()
     ]
   end
 
@@ -20,8 +21,22 @@ defmodule ExTectonicdb.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:dialyxir, "~> 1.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp dialyzer do
+    [
+      plt_file: {:no_warn, "_build/#{Mix.env()}/dialyzer.plt"},
+      flags: [
+        :unmatched_returns,
+        :error_handling,
+        :race_conditions,
+        :underspecs,
+        :no_opaque
+      ],
+      plt_add_deps: :transitive,
+      ignore_warnings: "dialyzer.ignore-warnings"
     ]
   end
 end
