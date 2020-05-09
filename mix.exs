858ce9700699f6d3cbf6b1db9903c654b4ef70ed
Yuri Kovalov <yuri@yurikoval.com>
2020-05-09 15:02:15 +0800

    Add more supported versions to testing

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExTectonicdb.MixProject do
     [
       app: :ex_tectonicdb,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: dialyzer()
